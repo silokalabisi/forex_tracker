a4012eada8f723136a93db285c526e791ea28b67
Labisi Siloka <silokaarthurl@gmail.com>
2024-12-01 20:25:10 +0800

    added yuan dollar exchange rate

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ API_KEY = os.getenv("Alpha_Vantage_API_key")
 base_url = 'https://www.alphavantage.co/query'
 
 # Define parameters
+
+# US Dollar
 params = {
     'function': 'CURRENCY_EXCHANGE_RATE',
     'from_currency': 'USD',
@@ -16,9 +18,26 @@ params = {
     'apikey': API_KEY
 }
 
+# Chinese Yuan
+paramsCH = {
+    'function': 'CURRENCY_EXCHANGE_RATE',
+    'from_currency': 'USD',
+    'to_currency': 'CNY',
+    'apikey': API_KEY
+}
+
+#  US
 response = requests.get(base_url, params=params)
 data = response.json()
 
+#  China
+responseCH = requests.get(base_url, params=paramsCH)
+dataCH = responseCH.json()
+
 # Extract the exchange rate
+# US
 exchange_rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
 print(f"Real-time USD/ZMW exchange rate: {exchange_rate}")
+# China
+exchange_rateCH = dataCH['Realtime Currency Exchange Rate']['5. Exchange Rate']
+print(f"Real-time USD/CNY exchange rate: {exchange_rateCH}")
